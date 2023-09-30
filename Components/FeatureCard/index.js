9970b4a655eb9d3aba4359c2ca3b4f85ef8f85a9
cpwallah <142053202+cpwallah@users.noreply.github.com>
2023-09-30 23:24:08 +0530

    Add files via upload

---
+++ Components/FeatureCard/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const FeatureCard = ({cards=[1,2,3]}) => {
+  return (
+    <section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-col text-center w-full mb-20">
+      <h2 class="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">ROOF PARTY POLAROID</h2>
+      <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900">Master Cleanse Reliac Heirloom</h1>
+    </div>
+    <div class="flex flex-wrap -m-4">
+      {
+      cards?.map((card)=>{
+        return(
+          <Link to={`/categories/${card}`} class="p-4 md:w-1/3 cursor-pointer">
+        <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+          <div class="flex items-center mb-3">
+            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+              </svg>
+            </div>
+            <h2 class="text-gray-900 text-lg title-font font-medium">{card || 'Example Card'}</h2>
+          </div>
+          <div class="flex-grow">
+            <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
+            <a class="mt-3 text-indigo-500 inline-flex items-center">Learn More
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </Link>
+        )
+      })
+    }
+      
+    </div>
+  </div>
+</section>
+  )
+}
+
+export default FeatureCard
