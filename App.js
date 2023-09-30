4a711c2bc94883af7492257feff96ee7a5080475
cpwallah <142053202+cpwallah@users.noreply.github.com>
2023-09-30 23:57:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+// import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Home from './modules/Home';
+import {Routes,Route} from 'react-router-dom';
+import Product from './modules/Product';
+import Products from './modules/Products';
+import CategoryProducts from './modules/CategoryProducts';
+import Cart from './modules/Cart';
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/products/:id" element={<Product/>}/>
+        <Route path="/products" element={<Products/>}/>
+        <Route path="/categories/:name" element={<CategoryProducts/>}/>
+        <Route path="cart" element={<Cart/>}/>
+        <Route path="*" element={<div>404</div>}/>
+      </Routes>
+      
+    </div>
+  );
+}
+
+export default App;
